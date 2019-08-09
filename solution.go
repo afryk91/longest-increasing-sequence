4f7167fc6fdd03fdba3ced1c895f5c2ef6691aa5
Krystian Ogierman <krystian.ogierman@gmail.com>
2019-08-09 11:49:33 +0200

    Fix helper arrays allocation in Go solution
    subsequenceEndingsIdxs and predecesorIdxs arrays were allocated with incorrect lengths

--- solution.go
+++ solution.go
@@ -9,9 +9,9 @@ func findLIS(list []int) []int {
 		return []int{}
 	}
 
-	var lengthOfLongest int = 1
-	var subsequenceEndingsIdxs = make([]int, len(list))
-	var predecesorIdxs = make([]int, len(list) + 1)
+	var lengthOfLongest int = 0
+	var subsequenceEndingsIdxs = make([]int, len(list) + 1)
+	var predecesorIdxs = make([]int, len(list))
 	
 
 	constructLIS := func () []int {
@@ -56,5 +56,19 @@ func findLIS(list []int) []int {
 
 func main() {
 	var input = []int{1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16}
-	fmt.Println(findLIS(input))
+	var increasing = []int{1,2,3,4,5,6}
+	var decreasing = []int{6,5,4,3,2,1}
+	var constant = []int{2,2,2,2,2,2}
+	var empty = []int{}
+	var repeating = []int{1,2,3,1,2,3,1,2,3}
+	var withNegative = []int{2,-1,3,6,4,8}
+	
+	
+	fmt.Println(findLIS(input), "should be [1,3,7,10,12,16]")
+	fmt.Println(findLIS(increasing), "should be [1,2,3,4,5,6]")
+	fmt.Println(findLIS(decreasing), "should be [1]")
+	fmt.Println(findLIS(constant), "should be [2]")
+	fmt.Println(findLIS(empty), "should be []")
+	fmt.Println(findLIS(repeating), "should be [1,2,3]")
+	fmt.Println(findLIS(withNegative), "should be [-1,3,4,8]")
 }
